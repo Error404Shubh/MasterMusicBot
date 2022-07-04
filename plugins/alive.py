38c30a80c6b244aa07156670c669afc5be32f1fe
SHUBHAM <44270581+Error404Shubh@users.noreply.github.com>
2022-07-05 00:27:42 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -34,7 +34,7 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        # photo=f"https://te.legra.ph/file/3752041b671e0afc6ada2.jpg",
+        photo=f"https://te.legra.ph/file/3752041b671e0afc6ada2.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━
 ✨ Hello I am very fast and Simple Music player bot  Developed By Shubham
 ━━━━━━━━━━━━━━━━━━━**""",
@@ -53,7 +53,7 @@ async def start_(client: Client, message: Message):
 )
     
     
-@Client.on_message(commandpro(["/start", "/alive", "Sumit", "/repo"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start", "/alive", "Shubham", "/repo"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
         caption=f"""""",
