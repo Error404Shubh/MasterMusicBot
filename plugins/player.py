deb0829cad27e2e03c9e8c361dca72866483be5e
SHUBHAM <44270581+Error404Shubh@users.noreply.github.com>
2022-07-05 01:44:50 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -87,7 +87,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: Sumit Yadav (@Simple_Mundaa)",
+        f"Powered By: Shubham Singh Rajput ",
         (255, 255, 255),
         font=font,
     )
